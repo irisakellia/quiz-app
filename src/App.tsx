68f3eda4066435381e46868ba4f2551d5be41649
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-10 11:53:58 +0200

    [adding more]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './App.css';
 import QuestionCard from './components/QuestionCard'
+import { useTheme } from 'styled-components';
 
-function App() {
+const TOTAL_QUESTIONS = 10 ;
+
+const App=()=> {
+  const[loading, setLoading] = useState(false);
+  const[questions, setQuestions] = useState([]);
+  const[number,setNumber] = useState(0);
+  const[userAnswers,setUserAnswers] = useState([]);
+  const[Score, setScore] = useState(0);
+  const [gameOver,setGameOver] = useState(true);
+
 
 const startTrivia = async ()=>{
 
@@ -22,7 +32,11 @@ const nextQuestion = ()=>{
   <button className='start' onClick={startTrivia}> Start</button>
   <p className='score'>Score:</p>
   <p>Loading Questions ...</p>
-<QuestionCard/>
+<QuestionCard 
+questionNr={number + 1 }
+totalQuestions={TOTAL_QUESTIONS}
+
+/>
 <button className='next' onClick={nextQuestion}>Next </button>
  </div>
   );
