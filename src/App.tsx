1af2b4a0d9eae46f1efd8047925f8da85a9b6937
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-10 11:14:55 +0200

    [adding more]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import QuestionCard from './components/QuestionCard'
 
 function App() {
-  return (
-  <div className='App'>Quiz
 
-  </div>
+const startTrivia = async ()=>{
+
+}
+
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>)=>{
+
+}
+
+const nextQuestion = ()=>{
+
+}
+
+  return (
+ <div className='App'>
+  <h1>REACT QUIZ</h1>
+  <button className='start' onClick={startTrivia}> Start</button>
+  <p className='score'>Score:</p>
+  <p>Loading Questions ...</p>
+<QuestionCard/>
+<button className='next' onClick={nextQuestion}>Next </button>
+ </div>
   );
 }
 
