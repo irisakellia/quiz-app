a35002b9b268c580fe768b9a79ec3fc23f47746b
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-10 12:42:32 +0200

    [adding more]

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React ,{useState}from 'react';
 import './App.css';
 import QuestionCard from './components/QuestionCard'
 import { useTheme } from 'styled-components';
+import {fetchQuizQuestions} from './images/API';
+import { Difficulty } from './images/API';
 
 const TOTAL_QUESTIONS = 10 ;
 
@@ -13,6 +15,7 @@ const App=()=> {
   const[Score, setScore] = useState(0);
   const [gameOver,setGameOver] = useState(true);
 
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY))
 
 const startTrivia = async ()=>{
 
@@ -32,11 +35,15 @@ const nextQuestion = ()=>{
   <button className='start' onClick={startTrivia}> Start</button>
   <p className='score'>Score:</p>
   <p>Loading Questions ...</p>
-<QuestionCard 
+{/* <QuestionCard 
 questionNr={number + 1 }
 totalQuestions={TOTAL_QUESTIONS}
+question = {questions[number].question}
+answers={questions[number].answers}
+userAnswer={userAnswers? userAnswers[number]: undefined}
+callback={checkAnswer}
 
-/>
+/> */}
 <button className='next' onClick={nextQuestion}>Next </button>
  </div>
   );
