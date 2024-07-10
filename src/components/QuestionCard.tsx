ca862e6a7520a180fe69251c02941ccc24ed30a9
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-10 11:22:32 +0200

    [adding more]

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+type Props = {
+    question:string ;
+    answer: string[];
+    callback:any;
+    userAnswer: string ;
+    questionNr:number;
+    totalQuestions:number ;
+}
+
+
+
+
+
+
+const QuestionCard: React.FC<Props> = ({question,answer,callback,userAnswer,questionNr,totalQuestions}) => {
+  return (
+    <div>
+      Question Card
+    </div>
+  )
+}
+
+export default QuestionCard
